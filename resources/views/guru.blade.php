4f10a195e515f16a33031aa8c0095af68511ab0d
DrPort <f.yudoyono@gmail.com>
2022-04-03 06:20:28 +0700

    Finished all navbar, dashboard for admin and teacher, and setup for other pages

---
+++ resources/views/guru.blade.php
@@ -0,0 +1,89 @@
+@extends('layouts.main')
+@section('content')
+    <div class="row">
+        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+          <div class="card card-statistic-1">
+            <div class="card-icon bg-primary">
+              <i class="fas fa-graduation-cap"></i>
+            </div>
+            <div class="card-wrap">
+              <div class="card-header">
+                <h4>Jumlah Siswa</h4>
+              </div>
+              <div class="card-body">
+                10
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+          <div class="card card-statistic-1">
+            <div class="card-icon bg-danger">
+              <i class="fas fa-door-open"></i>
+            </div>
+            <div class="card-wrap">
+              <div class="card-header">
+                <h4>Jumlah Kelas</h4>
+              </div>
+              <div class="card-body">
+                42
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+          <div class="card card-statistic-1">
+            <div class="card-icon bg-warning">
+              <i class="fas fa-book"></i>
+            </div>
+            <div class="card-wrap">
+              <div class="card-header">
+                <h4>Jumlah Mapel</h4>
+              </div>
+              <div class="card-body">
+                1,201
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6   col-sm-6 col-12">
+          <div class="card card-statistic-1">
+            <div class="card-icon bg-success">
+              <i class="fas fa-file"></i>
+            </div>
+            <div class="card-wrap">
+              <div class="card-header">
+                <h4>Jumlah Materi</h4>
+              </div>
+              <div class="card-body">
+                47
+              </div>
+            </div>
+          </div>
+        </div>                  
+    </div>
+
+      <div class="row">
+        <div class="col-12 col-md-6 col-lg-6">
+          <div class="card">
+            <div class="card-header">
+              <h4>Jumlah Murid Berdasarkan Jenis Kelamin</h4>
+            </div>
+            <div class="card-body"><div class="chartjs-size-monitor" style="position: absolute; inset: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;"><div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div></div><div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:200%;height:200%;left:0; top:0"></div></div></div>
+              <canvas id="myChart4" width="608" height="304" style="display: block; height: 152px; width: 304px;" class="chartjs-render-monitor"></canvas>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-6">
+          <div class="card">
+            <div class="card-header">
+              <h4>Jumlah Murid Berdasarkan Kelas</h4>
+            </div>
+            <div class="card-body"><div class="chartjs-size-monitor" style="position: absolute; inset: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;"><div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div></div><div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:200%;height:200%;left:0; top:0"></div></div></div>
+              <canvas id="myChart4" width="608" height="304" style="display: block; height: 152px; width: 304px;" class="chartjs-render-monitor"></canvas>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+@endsection
---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,30 @@
+
+<div class="main-sidebar sidebar-style-2">
+  <aside id="sidebar-wrapper">
+      <div class="sidebar-brand mt-3">
+        <a href="/"><img src="{{ asset('assets/img/logo.png')}}" alt="" width="25%"><br><br>SMK DHARMA BUDHI <br>BHAKTI PLUS</a>
+      </div>
+      <div class="sidebar-brand mt-3 sidebar-brand-sm">
+        <a href="/"><img src="{{ asset('assets/img/logo.png')}}" alt="" width="50%"></a>
+      </div>
+      <ul class="sidebar-menu mt-2">
+        <li class="menu-header">Dashboard</li>
+        <li><a class="nav-link" href="/dashboard-admin"><i class="fas fa-gauge-high"></i></i> <span>Dashboard Admin</span></a></li>
+        <li><a class="nav-link" href="/dashboard-guru"><i class="fas fa-gauge-simple-high"></i></i> <span>Dashboard Guru</span></a></li>
+
+        <li class="menu-header">Profil</li>
+        <li class="active"><a class="nav-link" href="/profil"><i class="fas fa-user"></i> <span>Profil</span></a></li>
+
+        <li class="menu-header">Data Input</li>
+        <li class="active"><a class="nav-link" href="/manajemen-user"><i class="fas fa-id-card"></i> <span>Manajemen User</span></a></li>
+        <li><a class="nav-link" href="/manajemen-kelas"><i class="fas fa-users-rectangle"></i> <span>Manajemen Kelas</span></a></li>
+        
+        <li class="menu-header">Menu Guru</li>        
+        <li><a class="nav-link" href="/manajemen-materi"><i class="fas fa-book"></i> <span>Manajemen Materi</span></a></li>
+        
+        <li class="menu-header">Menu Siswa</li>
+        <li><a class="nav-link" href="/materi-pembelajaran"><i class="fas fa-book-open"></i> <span>Materi Pembelajaran</span></a></li>  
+        <li><a class="nav-link" href="/pengumpulan-tugas"><i class="far fa-file-lines"></i> <span>Pengumpulan Tugas</span></a></li>
+        <li><a class="nav-link btn-danger text-white mb-5" href="/"><i class="fas fa-right-from-bracket"></i></i></i> <span>Logout</span></a></li>
+    </aside>
+</div>
